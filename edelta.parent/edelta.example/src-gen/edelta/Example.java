92b5a715f087a7990e5d62e418843b9469c4daf9
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-12-21 18:43:32 +0100

    example with operation

--- edelta.parent/edelta.example/src-gen/edelta/Example.java
+++ edelta.parent/edelta.example/src-gen/edelta/Example.java
@@ -1,5 +1,15 @@
 package edelta;
 
+import edelta.lib.EdeltaLibrary;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.xtext.xbase.lib.Extension;
+
 @SuppressWarnings("all")
 public class Example {
+  @Extension
+  private EdeltaLibrary lib;
+  
+  public EClass createClass(final String name) {
+    return this.lib.newEClass(name);
+  }
 }
