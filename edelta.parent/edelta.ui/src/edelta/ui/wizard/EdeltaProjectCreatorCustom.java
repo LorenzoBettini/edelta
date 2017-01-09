40938b9221279f1c023421a8c8195fc654d0a0b8
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-01-09 20:44:38 +0100

    add model as source folder

--- edelta.parent/edelta.ui/src/edelta/ui/wizard/EdeltaProjectCreatorCustom.java
+++ edelta.parent/edelta.ui/src/edelta/ui/wizard/EdeltaProjectCreatorCustom.java
@@ -5,6 +5,7 @@ package edelta.ui.wizard;
 
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 /**
@@ -16,5 +17,13 @@ public class EdeltaProjectCreatorCustom extends EdeltaProjectCreator {
 	@Override
 	protected List<String> getRequiredBundles() {
 		return Lists.newArrayList(EdeltaProjectCreator.DSL_PROJECT_NAME+".lib");
+	}
+
+	@Override
+	protected List<String> getAllFolders() {
+		List<String> allFolders = super.getAllFolders();
+		allFolders = Lists.newArrayList(allFolders);
+		allFolders.add("model");
+		return ImmutableList.copyOf(allFolders);
 	}
 }
