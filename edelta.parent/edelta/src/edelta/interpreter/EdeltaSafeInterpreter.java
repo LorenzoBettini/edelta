36bfe318269efa837eed1a0d92927f7339c6c5f9
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-05-22 12:07:59 +0200

    safe interpreter logs problems

--- edelta.parent/edelta/src/edelta/interpreter/EdeltaSafeInterpreter.java
+++ edelta.parent/edelta/src/edelta/interpreter/EdeltaSafeInterpreter.java
@@ -3,6 +3,7 @@
  */
 package edelta.interpreter;
 
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.xtext.common.types.JvmGenericType;
 import org.eclipse.xtext.xbase.interpreter.IEvaluationResult;
@@ -19,15 +20,26 @@ import edelta.edelta.EdeltaEcoreBaseEClassManipulationWithBlockExpression;
  */
 public class EdeltaSafeInterpreter implements IEdeltaInterpreter {
 
+	private static final Logger LOG = Logger.getLogger(EdeltaSafeInterpreter.class);
+
 	@Inject private EdeltaInterpreter delegate;
 
 	@Override
 	public IEvaluationResult run(EdeltaEcoreBaseEClassManipulationWithBlockExpression exp, EClass eClass,
 			JvmGenericType jvmGenericType) {
 		try {
-			return delegate.run(exp, eClass, jvmGenericType);
+			IEvaluationResult result = delegate.run(exp, eClass, jvmGenericType);
+			if (result != null) {
+				Throwable exception = result.getException();
+				if (exception != null) {
+					LOG.debug("result of interpreting", exception);
+					exception.printStackTrace();
+				}
+			}
+			return result;
 		} catch (RuntimeException e) {
-			// swallow runtime exceptions
+			LOG.debug("while interpreting", e);
+			e.printStackTrace();
 		}
 		return null;
 	}
