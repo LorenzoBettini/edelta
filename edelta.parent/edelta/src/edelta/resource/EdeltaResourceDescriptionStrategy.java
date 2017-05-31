0980086d162d28da4ef1a5a549130d39251dfc94
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-05-31 10:52:18 +0200

    don't index any EPackage at all

--- edelta.parent/edelta/src/edelta/resource/EdeltaResourceDescriptionStrategy.java
+++ edelta.parent/edelta/src/edelta/resource/EdeltaResourceDescriptionStrategy.java
@@ -4,12 +4,13 @@
 package edelta.resource;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.util.IAcceptor;
 import org.eclipse.xtext.xbase.resource.XbaseResourceDescriptionStrategy;
 
 /**
- * Don't index our derived state EPackages
+ * Don't index our EPackages created by the derived state computer
  * 
  * @author Lorenzo Bettini
  *
@@ -18,7 +19,7 @@ public class EdeltaResourceDescriptionStrategy extends XbaseResourceDescriptionS
 
 	@Override
 	public boolean createEObjectDescriptions(EObject eObject, IAcceptor<IEObjectDescription> acceptor) {
-		if (eObject instanceof EdeltaDerivedStateEPackage) {
+		if (eObject instanceof EPackage) {
 			return false;
 		}
 		return super.createEObjectDescriptions(eObject, acceptor);
