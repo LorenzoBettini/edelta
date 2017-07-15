44ac69f8f2a340e648120f83d97ce3b663268f4c
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-07-15 14:44:14 +0200

    travis build files

---
+++ travis_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+		echo "Build on MacOSX: Pull Request"
+		mvn -f edelta.parent/pom.xml clean verify
+	else
+		echo "Skipping build on MacOSX for standard commit"
+	fi
+else
+	echo "Build on Linux"
+	mvn -f edelta.parent/pom.xml clean verify -Pjacoco coveralls:report
+fi 
