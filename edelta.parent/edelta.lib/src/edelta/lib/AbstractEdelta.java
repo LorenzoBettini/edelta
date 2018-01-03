efc193e561a8a1c0fb5cd50cd8e97a99d24d628d
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2018-01-03 13:56:39 +0100

    avoid hardcoded path delimiter

--- edelta.parent/edelta.lib/src/edelta/lib/AbstractEdelta.java
+++ edelta.parent/edelta.lib/src/edelta/lib/AbstractEdelta.java
@@ -148,9 +148,9 @@ public abstract class AbstractEdelta {
 	public void saveModifiedEcores(String outputPath) throws IOException {
 		for (Entry<String, Resource> entry : packageManager.getResourceMapEntrySet()) {
 			Path p = Paths.get(entry.getKey());
-			String outputFileName = outputPath + "/" + p.getFileName().toString();
-			LOG.info("Saving " + outputFileName);
-			File newFile = new File(outputFileName);
+			final String fileName = p.getFileName().toString();
+			LOG.info("Saving " + outputPath + "/" + fileName);
+			File newFile = new File(outputPath, fileName);
 			FileOutputStream fos = new FileOutputStream(newFile);
 			entry.getValue().save(fos, null);
 			fos.flush();
