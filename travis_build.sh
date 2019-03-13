e0e14e0e9de436f868626431e7991ba9a181be73
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-03-13 19:03:46 +0100

    Build on Mac on pull requests

--- travis_build.sh
+++ travis_build.sh
@@ -4,7 +4,7 @@ set -ev
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
 		echo "Build on MacOSX: Pull Request"
-		mvn -f edelta.parent/pom.xml clean verify
+		mvn -f edelta.parent/pom.xml clean verify -Dtycho.disableP2Mirrors=true
 	else
 		echo "Skipping build on MacOSX for standard commit"
 	fi
