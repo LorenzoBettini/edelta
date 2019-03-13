60cf458be7e5db36a8a86f544fb90262cbd160f9
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-03-13 16:25:50 +0100

    33: SWTBot tests for the final Eclipse distribution
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/33

---
+++ edelta.parent/edelta.swtbot.tests/src/edelta/swtbot/tests/EdeltaSwtBotTest.java
@@ -0,0 +1,20 @@
+package edelta.swtbot.tests;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Lorenzo Bettini
+ *
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class EdeltaSwtBotTest extends EdeltaAbstractSwtbotTest {
+
+	@Test
+	public void canCreateANewProject() throws CoreException {
+		createProjectAndAssertNoErrorMarker();
+	}
+
+}
