2386d885962e809fe2f1ec0ed2ff69b7ccb538c7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-12-03 08:30:47 +0100

    created project personlist example

---
+++ edelta.parent/edelta.personlist.example/src/com/example/MainSyntax1.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import edelta.lib.AbstractEdelta;
+
+public class MainSyntax1 {
+
+	public static void main(String[] args) throws Exception {
+		// Create an instance of the generated Java class
+		AbstractEdelta edelta = new ExampleSyntax1();
+		// Make sure you load all the used Ecores
+		edelta.loadEcoreFile("model/PersonList.ecore");
+		// Execute the actual transformations defined in the DSL
+		edelta.execute();
+		// Save the modified Ecore model into a new path
+		edelta.saveModifiedEcores("modified-syntax1");
+	}
+}
