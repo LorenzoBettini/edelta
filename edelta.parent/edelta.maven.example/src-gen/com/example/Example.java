d2af6ab35b77c7d87710d17c5c6ec51d65608b39
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-12-12 17:54:03 +0100

    pure Maven example works!
    xtext-maven-plugin with EcoreSupport

--- edelta.parent/edelta.maven.example/src-gen/com/example/Example.java
+++ edelta.parent/edelta.maven.example/src-gen/com/example/Example.java
@@ -3,7 +3,6 @@ package com.example;
 import edelta.lib.AbstractEdelta;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
@@ -22,9 +21,23 @@ public class Example extends AbstractEdelta {
     final Procedure1<EClass> _function = new Procedure1<EClass>() {
       public void apply(final EClass it) {
         EList<EClass> _eSuperTypes = it.getESuperTypes();
-        _eSuperTypes.add(EcorePackage.Literals.ECLASS);
+        _eSuperTypes.add(getEClass("ecore", "EClass"));
       }
     };
     return ObjectExtensions.<EClass>operator_doubleArrow(_newEClass, _function);
+  }
+  
+  @Override
+  public void performSanityChecks() throws Exception {
+    ensureEPackageIsLoaded("myecore");
+    ensureEPackageIsLoaded("ecore");
+  }
+  
+  @Override
+  protected void doExecute() throws Exception {
+    createEClass("myecore", "MyNewClass", createList(this::_createEClass_MyNewClass_in_myecore));
+  }
+  
+  public void _createEClass_MyNewClass_in_myecore(final EClass it) {
   }
 }
