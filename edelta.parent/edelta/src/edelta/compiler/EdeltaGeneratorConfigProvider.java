39bb5048857daeb80671e289c498206450440a2a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-12-13 11:44:15 +0100

    60: Custom GeneratorConfigProvider
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/60

---
+++ edelta.parent/edelta/src/edelta/compiler/EdeltaGeneratorConfigProvider.java
@@ -0,0 +1,26 @@
+package edelta.compiler;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.util.JavaVersion;
+import org.eclipse.xtext.xbase.compiler.GeneratorConfig;
+import org.eclipse.xtext.xbase.compiler.GeneratorConfigProvider;
+import org.eclipse.xtext.xbase.compiler.XbaseCompiler;
+
+/**
+ * In order to make {@link XbaseCompiler} generate Java 8 lambdas also when
+ * running from xtext-maven-plugin.
+ * 
+ * @see https://github.com/LorenzoBettini/edelta/issues/60
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
+public class EdeltaGeneratorConfigProvider extends GeneratorConfigProvider {
+
+	@Override
+	public GeneratorConfig get(EObject context) {
+		final GeneratorConfig generatorConfig = super.get(context);
+		generatorConfig.setJavaSourceVersion(JavaVersion.JAVA8);
+		return generatorConfig;
+	}
+}
