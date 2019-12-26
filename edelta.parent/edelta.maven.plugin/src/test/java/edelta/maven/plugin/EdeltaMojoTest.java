c57fff7d2422498db683636df6d4c872f3ccce8f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-12-26 17:37:58 +0100

    80: refactored tests
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/80

--- edelta.parent/edelta.maven.plugin/src/test/java/edelta/maven/plugin/EdeltaMojoTest.java
+++ edelta.parent/edelta.maven.plugin/src/test/java/edelta/maven/plugin/EdeltaMojoTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.testing.MojoRule;
+import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.junit.Rule;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 public class EdeltaMojoTest {
 	@Rule
@@ -24,70 +32,76 @@ public class EdeltaMojoTest {
 
 	private static final String TEST_PROJECTS_DIR = "target/test-classes";
 
-	@Test
-	public void testProjectWithoutSources() throws Exception {
-		File pomPath = new File(TEST_PROJECTS_DIR + "/project-without-sources/");
+	private static final List<String> generatedDirs = Lists.newArrayList("target", "edelta-gen", "alt-gen");
+
+	private static void cleanDirectory(File baseDir) throws IOException {
+		for (File file : baseDir.listFiles())
+			if (generatedDirs.contains(file.getName()))
+				FileUtils.deleteDirectory(file);
+	}
+
+	private File setupPom(String testProjectDir) throws IOException {
+		File pomPath = new File(TEST_PROJECTS_DIR + testProjectDir);
 		assertNotNull(pomPath);
 		assertTrue(pomPath.exists());
+		cleanDirectory(pomPath);
+		return pomPath;
+	}
 
-		EdeltaMojo EdeltaMojo = (EdeltaMojo) rule.lookupConfiguredMojo(pomPath, "generate");
-		assertNotNull(EdeltaMojo);
-		EdeltaMojo.execute();
+	private EdeltaMojo executeMojo(File pomPath)
+			throws Exception, ComponentConfigurationException, MojoExecutionException, MojoFailureException {
+		EdeltaMojo edeltaMojo = (EdeltaMojo) rule.lookupConfiguredMojo(pomPath, "generate");
+		assertNotNull(edeltaMojo);
+		edeltaMojo.execute();
+		return edeltaMojo;
+	}
 
-		File outputDirectory = new File(rule.getVariableValueFromObject(EdeltaMojo, "outputDirectory").toString());
+	@Test
+	public void testProjectWithoutSources() throws Exception {
+		File pomPath = setupPom("/project-without-sources/");
+		EdeltaMojo edeltaMojo = executeMojo(pomPath);
+
+		File outputDirectory = new File(rule.getVariableValueFromObject(edeltaMojo, "outputDirectory").toString());
 		assertNotNull(outputDirectory);
 		assertFalse(outputDirectory.exists());
 
-		File xtextTmpDirectory = new File(rule.getVariableValueFromObject(EdeltaMojo, "tmpClassDirectory").toString());
+		File xtextTmpDirectory = new File(rule.getVariableValueFromObject(edeltaMojo, "tmpClassDirectory").toString());
 		assertNotNull(xtextTmpDirectory);
 		assertTrue(xtextTmpDirectory.exists());
 	}
 
+
+
 	@Test
 	public void testProjectWithoutEcore() throws Exception {
-		File pomPath = new File(TEST_PROJECTS_DIR + "/project-without-ecore/");
-		assertNotNull(pomPath);
-		assertTrue(pomPath.exists());
-
-		EdeltaMojo EdeltaMojo = (EdeltaMojo) rule.lookupConfiguredMojo(pomPath, "generate");
-		assertNotNull(EdeltaMojo);
-		EdeltaMojo.execute();
+		File pomPath = setupPom("/project-without-ecore/");
+		EdeltaMojo edeltaMojo = executeMojo(pomPath);
 
 		File outputDirectory = 
 			new File(
 				pomPath.getAbsolutePath(),
-				rule.getVariableValueFromObject(EdeltaMojo, "outputDirectory").toString());
+				rule.getVariableValueFromObject(edeltaMojo, "outputDirectory").toString());
 		assertNotNull(outputDirectory);
 		assertTrue(outputDirectory.exists());
 	}
 
 	@Test
 	public void testProjectWithEcore() throws Exception {
-		File pomPath = new File(TEST_PROJECTS_DIR + "/project-with-ecore/");
-		assertNotNull(pomPath);
-		assertTrue(pomPath.exists());
-
-		EdeltaMojo EdeltaMojo = (EdeltaMojo) rule.lookupConfiguredMojo(pomPath, "generate");
-		assertNotNull(EdeltaMojo);
-		EdeltaMojo.execute();
+		File pomPath = setupPom("/project-with-ecore/");
+		EdeltaMojo edeltaMojo = executeMojo(pomPath);
 
 		File outputDirectory = 
 			new File(
 				pomPath.getAbsolutePath(),
-				rule.getVariableValueFromObject(EdeltaMojo, "outputDirectory").toString());
+				rule.getVariableValueFromObject(edeltaMojo, "outputDirectory").toString());
 		assertNotNull(outputDirectory);
 		assertTrue(outputDirectory.exists());
 	}
 
 	@Test
 	public void testProjectWithOutputDirectory() throws Exception {
-		File pomPath = new File(TEST_PROJECTS_DIR + "/project-with-output-directory/");
-		assertNotNull(pomPath);
-		assertTrue(pomPath.exists());
-
-		EdeltaMojo EdeltaMojo = (EdeltaMojo) rule.lookupConfiguredMojo(pomPath, "generate");
-		assertNotNull(EdeltaMojo);
-		EdeltaMojo.execute();
+		File pomPath = setupPom("/project-with-output-directory/");
+		executeMojo(pomPath);
 
 		File defaultOutputDirectory = 
 			new File(
