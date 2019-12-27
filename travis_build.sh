0f414ef775c7e3b5c7b480a66c0bbbb9598b21b6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-12-27 19:04:20 +0100

    80: Travis clean install
    to make ITs work
    
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/80

--- travis_build.sh
+++ travis_build.sh
@@ -4,13 +4,13 @@ set -ev
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
 		echo "Build on MacOSX: Pull Request"
-		./mvnw -f edelta.parent/pom.xml clean verify $ADDITIONAL
+		./mvnw -f edelta.parent/pom.xml clean install $ADDITIONAL
 	else
 		echo "Skipping build on MacOSX for standard commit"
 	fi
 else
 	echo "Build on Linux"
-	./mvnw -f edelta.parent/pom.xml clean verify $ADDITIONAL
+	./mvnw -f edelta.parent/pom.xml clean install $ADDITIONAL
 fi 
 
 #  -Dtycho.disableP2Mirrors=true
