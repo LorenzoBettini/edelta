2b8daddad8d903f8ceb5edd91aad11fef4a6671c
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-01-10 17:54:29 +0100

    83: updated template project
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/83

--- edelta.parent/edelta.newproject.template/src/com/example/Main.java
+++ edelta.parent/edelta.newproject.template/src/com/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		// Create an instance of the generated Java class
 		AbstractEdelta edelta = new Example();
-		// Make sure you load all the used Ecores
+		// Make sure you load all the used Ecores (Ecore.ecore is always loaded)
 		edelta.loadEcoreFile("model/My.ecore");
 		// Execute the actual transformations defined in the DSL
 		edelta.execute();
