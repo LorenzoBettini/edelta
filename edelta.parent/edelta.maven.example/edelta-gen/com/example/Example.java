7f379c0930f7b6c8f208d296ad0ec99653370146
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-05-06 18:07:40 +0200

    130: Maven example with reusable function
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/130

--- edelta.parent/edelta.maven.example/edelta-gen/com/example/Example.java
+++ edelta.parent/edelta.maven.example/edelta-gen/com/example/Example.java
@@ -1,35 +1,32 @@
 package com.example;
 
+import com.example.ExampleReusableFunctions;
 import edelta.lib.AbstractEdelta;
 import edelta.refactorings.lib.EdeltaRefactorings;
 import java.util.function.Consumer;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.xtext.xbase.lib.Extension;
-import org.eclipse.xtext.xbase.lib.ObjectExtensions;
-import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
 @SuppressWarnings("all")
 public class Example extends AbstractEdelta {
   @Extension
   private EdeltaRefactorings refactorings;
   
+  @Extension
+  private ExampleReusableFunctions myfunctions;
+  
   public Example() {
     refactorings = new EdeltaRefactorings(this);
+    myfunctions = new ExampleReusableFunctions(this);
   }
   
   public Example(final AbstractEdelta other) {
     super(other);
   }
   
-  public EClass createClass(final String name) {
-    EClass _newEClass = this.lib.newEClass(name);
-    final Procedure1<EClass> _function = (EClass it) -> {
-      EList<EClass> _eSuperTypes = it.getESuperTypes();
-      _eSuperTypes.add(getEClass("myecore", "MyEClass"));
-    };
-    return ObjectExtensions.<EClass>operator_doubleArrow(_newEClass, _function);
+  public EClass createSubClassOfMyEClass(final String name) {
+    return this.myfunctions.createClassWithSubClass(name, getEClass("myecore", "MyEClass"));
   }
   
   public void aModification(final EPackage it) {
@@ -38,7 +35,7 @@ public class Example extends AbstractEdelta {
       getEAttribute("myecore", "MyNewClass", "ANewAttribute").setEType(getEDataType("ecore", "EInt"));
     };
     this.lib.addNewEClass(it, "MyNewClass", _function);
-    this.lib.addEClass(it, this.createClass("ANewDerivedEClass"));
+    this.lib.addEClass(it, this.createSubClassOfMyEClass("ANewDerivedEClass"));
     getEClass("myecore", "ANewDerivedEClass").setAbstract(true);
   }
   
