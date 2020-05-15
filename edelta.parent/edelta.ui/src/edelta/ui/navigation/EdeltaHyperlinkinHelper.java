6312f247e70349d1f4869891c8fa352104417a4a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-05-15 13:38:26 +0200

    164: formatting
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/164

--- edelta.parent/edelta.ui/src/edelta/ui/navigation/EdeltaHyperlinkinHelper.java
+++ edelta.parent/edelta.ui/src/edelta/ui/navigation/EdeltaHyperlinkinHelper.java
@@ -3,11 +3,12 @@
  */
 package edelta.ui.navigation;
 
+import static org.eclipse.xtext.EcoreUtil2.getContainerOfType;
+import static org.eclipse.xtext.nodemodel.util.NodeModelUtils.findActualSemanticObjectFor;
+
 import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.nodemodel.INode;
-import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkAcceptor;
 import org.eclipse.xtext.xbase.XExpression;
@@ -33,7 +34,7 @@ public class EdeltaHyperlinkinHelper extends XbaseHyperLinkHelper {
 
 	@Override
 	protected void createHyperlinksTo(XtextResource resource, INode node, EObject target, IHyperlinkAcceptor acceptor) {
-		EObject semanticObj = NodeModelUtils.findActualSemanticObjectFor(node);
+		EObject semanticObj = findActualSemanticObjectFor(node);
 		if (semanticObj instanceof EdeltaEcoreReference) {
 			final EdeltaEcoreReference ecoreReference = (EdeltaEcoreReference) semanticObj;
 			final ENamedElement original = edeltaDerivedStateHelper
@@ -45,7 +46,8 @@ public class EdeltaHyperlinkinHelper extends XbaseHyperLinkHelper {
 			}
 			XExpression expression = edeltaDerivedStateHelper
 				.getEcoreReferenceExpressionState(
-						EcoreUtil2.getContainerOfType(ecoreReference, EdeltaEcoreReferenceExpression.class))
+						getContainerOfType(ecoreReference,
+								EdeltaEcoreReferenceExpression.class))
 				.getEnamedElementXExpressionMap()
 				.get(ecoreReference.getEnamedelement());
 			super.createHyperlinksTo(resource, node, expression, acceptor);
