83264a6007e9d19401e0a9ff751ef5fcc7c22e5f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-06-05 17:23:32 +0200

    190: compiler util in Java
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/190

---
+++ edelta.parent/edelta/src/edelta/compiler/EdeltaCompilerUtil.java
@@ -0,0 +1,63 @@
+package edelta.compiler;
+
+import static org.eclipse.xtext.xbase.lib.CollectionLiterals.newArrayList;
+import static org.eclipse.xtext.xbase.lib.IterableExtensions.filterNull;
+import static org.eclipse.xtext.xbase.lib.IterableExtensions.toList;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+import org.eclipse.emf.ecore.EPackage;
+
+import com.google.inject.Inject;
+
+import edelta.edelta.EdeltaEcoreReference;
+import edelta.edelta.EdeltaEcoreReferenceExpression;
+import edelta.resource.derivedstate.EdeltaEcoreReferenceState;
+import edelta.util.EdeltaEcoreReferenceInformationHelper;
+
+/**
+ * Utilities for Edelta compiler
+ * 
+ * @author Lorenzo Bettini
+ */
+public class EdeltaCompilerUtil {
+	@Inject
+	private EdeltaEcoreReferenceInformationHelper edeltaEcoreReferenceInformationHelper;
+
+	public String getEPackageNameOrNull(final EPackage e) {
+		if (e != null) {
+			return e.getName();
+		}
+		return null;
+	}
+
+	public String getStringForEcoreReferenceExpression(final EdeltaEcoreReferenceExpression e) {
+		final EdeltaEcoreReference reference = e.getReference();
+		if (reference == null || reference.getEnamedelement() == null) {
+			return "null";
+		}
+		return buildMethodToCallForEcoreReference(e,
+			(name, args) -> {
+				final StringBuilder builder = new StringBuilder();
+				builder.append(name);
+				builder.append("(");
+				builder.append(args.stream()
+					.map(it -> "\"" + it + "\"")
+					.collect(Collectors.joining(", ")));
+				builder.append(")");
+				return builder.toString();
+			});
+	}
+
+	public <T extends Object> T buildMethodToCallForEcoreReference(final EdeltaEcoreReferenceExpression e,
+			final BiFunction<String, List<Object>, T> function) {
+		final EdeltaEcoreReferenceState.EdeltaEcoreReferenceStateInformation info =
+			edeltaEcoreReferenceInformationHelper.getOrComputeInformation(e);
+		return function.apply(
+			"get" + info.getType(),
+			toList(filterNull(newArrayList(
+				info.getEPackageName(), info.getEClassifierName(), info.getENamedElementName()))));
+	}
+}
