cbc99a4da10f4a86610b2d7055e09241d9c36dbd
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-06-08 19:23:37 +0200

    186: example
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/186

---
+++ edelta.parent/edelta.example/edelta-gen/edelta/ExampleAcrossEPackages.java
@@ -0,0 +1,38 @@
+package edelta;
+
+import edelta.lib.AbstractEdelta;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+
+@SuppressWarnings("all")
+public class ExampleAcrossEPackages extends AbstractEdelta {
+  public ExampleAcrossEPackages() {
+    
+  }
+  
+  public ExampleAcrossEPackages(final AbstractEdelta other) {
+    super(other);
+  }
+  
+  public void aTest1(final EPackage it) {
+    EReference _eOpposite = getEReference("testecoreforreferences1", "Person", "works").getEOpposite();
+    _eOpposite.setName("renamedPersons");
+  }
+  
+  public void aTest2(final EPackage it) {
+    EReference _eOpposite = getEReference("testecoreforreferences2", "WorkPlace", "renamedPersons").getEOpposite();
+    _eOpposite.setName("renamedWorks");
+  }
+  
+  @Override
+  public void performSanityChecks() throws Exception {
+    ensureEPackageIsLoaded("testecoreforreferences1");
+    ensureEPackageIsLoaded("testecoreforreferences2");
+  }
+  
+  @Override
+  protected void doExecute() throws Exception {
+    aTest1(getEPackage("testecoreforreferences1"));
+    aTest2(getEPackage("testecoreforreferences2"));
+  }
+}
