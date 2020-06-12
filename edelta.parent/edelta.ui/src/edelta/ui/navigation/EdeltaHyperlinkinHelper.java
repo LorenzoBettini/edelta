4f74d5a6cc9626ce773ba0cca1feac08698668ec
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-06-12 16:14:02 +0200

    199: updated EdeltaHyperlinkinHelper
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/199

--- edelta.parent/edelta.ui/src/edelta/ui/navigation/EdeltaHyperlinkinHelper.java
+++ edelta.parent/edelta.ui/src/edelta/ui/navigation/EdeltaHyperlinkinHelper.java
@@ -5,7 +5,6 @@ package edelta.ui.navigation;
 
 import static org.eclipse.xtext.nodemodel.util.NodeModelUtils.findActualSemanticObjectFor;
 
-import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
@@ -16,7 +15,7 @@ import org.eclipse.xtext.xbase.ui.navigation.XbaseHyperLinkHelper;
 import com.google.inject.Inject;
 
 import edelta.edelta.EdeltaEcoreReference;
-import edelta.resource.derivedstate.EdeltaDerivedStateHelper;
+import edelta.navigation.EdeltaNavigationTargetHelper;
 
 /**
  * Customizations for {@link EdeltaEcoreReference}: jump to the original element
@@ -28,24 +27,16 @@ import edelta.resource.derivedstate.EdeltaDerivedStateHelper;
 public class EdeltaHyperlinkinHelper extends XbaseHyperLinkHelper {
 
 	@Inject
-	private EdeltaDerivedStateHelper edeltaDerivedStateHelper;
+	private EdeltaNavigationTargetHelper navigationTargetHelper;
 
 	@Override
 	protected void createHyperlinksTo(XtextResource resource, INode node, EObject target, IHyperlinkAcceptor acceptor) {
 		EObject semanticObj = findActualSemanticObjectFor(node);
-		if (semanticObj instanceof EdeltaEcoreReference) {
-			final EdeltaEcoreReference ecoreReference = (EdeltaEcoreReference) semanticObj;
-			final ENamedElement original = edeltaDerivedStateHelper
-					.getOriginalEnamedelement(ecoreReference);
-			if (original != null) {
-				super.createHyperlinksTo(resource, node, original, acceptor);
-				return;
-			}
-			XExpression expression = edeltaDerivedStateHelper
-				.getResponsibleExpression(ecoreReference);
-			super.createHyperlinksTo(resource, node, expression, acceptor);
-			return;
+		final var effectiveTarget = navigationTargetHelper.getTarget(semanticObj);
+		if (effectiveTarget != null) {
+			super.createHyperlinksTo(resource, node, effectiveTarget, acceptor);
+		} else {
+			super.createHyperlinksTo(resource, node, target, acceptor);
 		}
-		super.createHyperlinksTo(resource, node, target, acceptor);
 	}
 }
