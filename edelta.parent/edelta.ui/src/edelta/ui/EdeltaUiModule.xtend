2cef6a5be0db7d1d390f63f96fdeb1d8085911f4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-07-24 19:15:06 +0200

    227: first impl Link from XExpression to Outline
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/227

--- edelta.parent/edelta.ui/src/edelta/ui/EdeltaUiModule.xtend
+++ edelta.parent/edelta.ui/src/edelta/ui/EdeltaUiModule.xtend
@@ -1,7 +1,9 @@
 package edelta.ui
 
-import org.eclipse.xtend.lib.annotations.FinalFieldsConstructor
 import edelta.ui.navigation.EdeltaHyperlinkinHelper
+import org.eclipse.xtend.lib.annotations.FinalFieldsConstructor
+import org.eclipse.xtext.ui.editor.outline.actions.OutlineWithEditorLinker
+import edelta.ui.outline.actions.EdeltaOutlineWithEditorLinker
 
 /**
  * Use this class to register components to be used within the Eclipse IDE.
@@ -13,4 +15,7 @@ class EdeltaUiModule extends AbstractEdeltaUiModule {
 		EdeltaHyperlinkinHelper
 	}
 
+	def Class<? extends OutlineWithEditorLinker> bindOutlineWithEditorLinker() {
+		EdeltaOutlineWithEditorLinker
+	}
 }
---
+++ edelta.parent/edelta.ui/src/edelta/ui/outline/actions/EdeltaOutlineWithEditorLinker.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package edelta.ui.outline.actions;
+
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.xtext.ui.editor.outline.IOutlineNode;
+import org.eclipse.xtext.ui.editor.outline.actions.OutlineWithEditorLinker;
+import org.eclipse.xtext.ui.editor.outline.impl.EObjectNode;
+import org.eclipse.xtext.util.ITextRegion;
+import org.eclipse.xtext.xbase.XExpression;
+
+import edelta.edelta.EdeltaPackage;
+
+/**
+ * From an {@link XExpression} responsible of the modification of an {@link ENamedElement}
+ * in the editor, navigate to the corresponding element in the outline.
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
+public class EdeltaOutlineWithEditorLinker extends OutlineWithEditorLinker {
+
+	@Override
+	protected IOutlineNode findBestNode(IOutlineNode input, ITextRegion selectedTextRegion) {
+		final var findBestNode = super.findBestNode(input, selectedTextRegion);
+		if (findBestNode instanceof EObjectNode) {
+			EObjectNode eObjectNode = (EObjectNode) findBestNode;
+			if (eObjectNode.getEClass() == EdeltaPackage.Literals.EDELTA_MODIFY_ECORE_OPERATION) {
+				final var findENamedElementNode = findENamedElementNode(eObjectNode.getParent(), selectedTextRegion);
+				if (findENamedElementNode != null)
+					return findENamedElementNode;
+			}
+		}
+		return findBestNode;
+	}
+
+	private IOutlineNode findENamedElementNode(IOutlineNode node, ITextRegion selectedTextRegion) {
+		for (var child : node.getChildren()) {
+			if (child instanceof EObjectNode) {
+				EObjectNode eObjectNode = (EObjectNode) child;
+				if (EcorePackage.Literals.ENAMED_ELEMENT.isSuperTypeOf(eObjectNode.getEClass())) {
+					if (eObjectNode.getSignificantTextRegion().contains(selectedTextRegion)) {
+						return eObjectNode;
+					}
+					final var recursiveFind = findENamedElementNode(child, selectedTextRegion);
+					if (recursiveFind != null)
+						return recursiveFind;
+				}
+			}
+		}
+		return null;
+	}
+}
