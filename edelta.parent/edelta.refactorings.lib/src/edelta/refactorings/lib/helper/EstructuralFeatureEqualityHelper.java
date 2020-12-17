ebe6ab15a6f8a39f3fad371e7b2e4b07fd586b4f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-12-17 14:31:56 +0100

    266: Our EqualityHelper delegates to a custom EMF EqualityHelper
    instead of extending it.
    
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/266

--- edelta.parent/edelta.refactorings.lib/src/edelta/refactorings/lib/helper/EstructuralFeatureEqualityHelper.java
+++ edelta.parent/edelta.refactorings.lib/src/edelta/refactorings/lib/helper/EstructuralFeatureEqualityHelper.java
@@ -10,16 +10,16 @@ import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.util.EcoreUtil.EqualityHelper;
 
 /**
- * Custom implementation that does not consider eContainingClass when checking
- * for equality (since we want to check whether features in different classes
- * can be considered the same); it also stores and make available, in case
- * of non equality, the {@link EStructuralFeature} that made the equality fail.
+ * Relies on a custom implementation of {@link EqualityHelper} that does not
+ * consider eContainingClass when checking for equality (since we want to check
+ * whether features in different classes can be considered the same); it also
+ * stores and make available, in case of non equality, the
+ * {@link EStructuralFeature} that made the equality fail.
  * 
  * @author Lorenzo Bettini
  *
  */
-@SuppressWarnings("serial")
-public class EstructuralFeatureEqualityHelper extends EqualityHelper {
+public class EstructuralFeatureEqualityHelper {
 
 	private EStructuralFeature difference;
 
@@ -27,20 +27,28 @@ public class EstructuralFeatureEqualityHelper extends EqualityHelper {
 		return difference;
 	}
 
-	@Override
-	protected boolean haveEqualFeature(EObject eObject1, EObject eObject2, EStructuralFeature feature) {
-		boolean haveEqualFeature = super.haveEqualFeature(eObject1, eObject2, feature);
-		if (!haveEqualFeature) {
-			this.difference = feature;
-		}
-		return haveEqualFeature;
+	public boolean equals(EObject eObject1, EObject eObject2) {
+		return new EqualityHelper() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected boolean haveEqualFeature(EObject eObject1, EObject eObject2, EStructuralFeature feature) {
+				boolean haveEqualFeature = super.haveEqualFeature(eObject1, eObject2, feature);
+				if (!haveEqualFeature) {
+					difference = feature;
+				}
+				return haveEqualFeature;
+			}
+
+			@Override
+			protected boolean haveEqualReference(EObject eObject1, EObject eObject2, EReference reference) {
+				if (reference.equals(EcorePackage.Literals.ESTRUCTURAL_FEATURE__ECONTAINING_CLASS)) {
+					return true;
+				}
+				return super.haveEqualReference(eObject1, eObject2, reference);
+			}
+
+		}.equals(eObject1, eObject2);
 	}
 
-	@Override
-	protected boolean haveEqualReference(EObject eObject1, EObject eObject2, EReference reference) {
-		if (reference.equals(EcorePackage.Literals.ESTRUCTURAL_FEATURE__ECONTAINING_CLASS)) {
-			return true;
-		}
-		return super.haveEqualReference(eObject1, eObject2, reference);
-	}
 }
