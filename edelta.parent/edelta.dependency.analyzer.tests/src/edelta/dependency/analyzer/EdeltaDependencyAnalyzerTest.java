1ff26b4d951a8bf54f8ef6f35a9f3f1722871e2e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-05-24 19:34:52 +0200

    331: highlight even target dependency
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/331

--- edelta.parent/edelta.dependency.analyzer.tests/src/edelta/dependency/analyzer/EdeltaDependencyAnalyzerTest.java
+++ edelta.parent/edelta.dependency.analyzer.tests/src/edelta/dependency/analyzer/EdeltaDependencyAnalyzerTest.java
@@ -1,6 +1,9 @@
 package edelta.dependency.analyzer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.IOException;
 
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.Test;
@@ -22,7 +25,7 @@ public class EdeltaDependencyAnalyzerTest {
 		assertThat(package1).isNotNull();
 		assertThat(package2).isNotNull();
 		// package1 uses package2
-		var p1Repository = analyzer.analyzeEPackage(package1);
+		var p1Repository = analyzer.analyzeMainEPackage(package1);
 		var metamodels = assertThat(p1Repository.getNodes())
 			.asInstanceOf(InstanceOfAssertFactories.list(Metamodel.class));
 		metamodels
@@ -31,6 +34,56 @@ public class EdeltaDependencyAnalyzerTest {
 		metamodels
 			.extracting(Metamodel::getNsURI)
 			.containsExactly(package1.getNsURI(), package2.getNsURI());
+		metamodels
+			.extracting(Metamodel::isHighlighted)
+			.containsExactly(true, false);
+		var dependencies = assertThat(p1Repository.getEdges())
+			.asInstanceOf(InstanceOfAssertFactories.list(Dependency.class));
+		dependencies
+			.extracting(d -> d.getSrc().getName())
+			.containsExactly(package1.getName());
+		dependencies
+			.extracting(d -> d.getTrg().getName())
+			.containsExactly(package2.getName());
+		dependencies
+			.extracting(Dependency::isBidirectional)
+			.containsExactly(false);
+//		assertThat(EdeltaLibrary.usedPackages(package1))
+//			.containsExactlyInAnyOrder(package2);
+//		assertThat(EdeltaLibrary.usedPackages(package2))
+//			.containsExactlyInAnyOrder(package1);
+//		assertThat(EdeltaLibrary.usedPackages(package3))
+//			.containsExactlyInAnyOrder(package2);
+//		assertThat(EdeltaLibrary.usedPackages(package4))
+//			.containsExactlyInAnyOrder(package2, package1);
+	}
+
+	@Test
+	public void testEPackageNotFound() throws IOException {
+		var analyzer = new EdeltaDependencyAnalizer();
+		assertThatThrownBy(
+			() -> analyzer.analyzeEPackage(TESTECORES + "/unidirectional/", "nonexistent"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("No EPackage with name: nonexistent");
+	}
+
+	@Test
+	public void testHighlightedTargetDependency() throws IOException {
+		var analyzer = new EdeltaDependencyAnalizer();
+		// package1 uses package2
+		var p1Repository = analyzer.analyzeEPackage(TESTECORES + "/unidirectional/", "testecoreforusages2");
+		var package1 = analyzer.getEPackage("testecoreforusages1");
+		var package2 = analyzer.getEPackage("testecoreforusages2");
+		assertThat(package1).isNotNull();
+		assertThat(package2).isNotNull();
+		var metamodels = assertThat(p1Repository.getNodes())
+			.asInstanceOf(InstanceOfAssertFactories.list(Metamodel.class));
+		metamodels
+			.extracting(Metamodel::getName)
+			.containsExactly(package2.getName(), package1.getName());
+		metamodels
+			.extracting(Metamodel::getNsURI)
+			.containsExactly(package2.getNsURI(), package1.getNsURI());
 		metamodels
 			.extracting(Metamodel::isHighlighted)
 			.containsExactly(true, false);
@@ -66,7 +119,7 @@ public class EdeltaDependencyAnalyzerTest {
 		assertThat(package2).isNotNull();
 		// package1 uses package2
 		// package2 uses package1
-		var p1Repository = analyzer.analyzeEPackage(package1);
+		var p1Repository = analyzer.analyzeMainEPackage(package1);
 		var metamodels = assertThat(p1Repository.getNodes())
 			.asInstanceOf(InstanceOfAssertFactories.list(Metamodel.class));
 		metamodels
--- edelta.parent/edelta.dependency.analyzer/src/edelta/dependency/analyzer/EdeltaDependencyAnalizer.java
+++ edelta.parent/edelta.dependency.analyzer/src/edelta/dependency/analyzer/EdeltaDependencyAnalizer.java
@@ -1,5 +1,13 @@
 package edelta.dependency.analyzer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.eclipse.emf.ecore.EPackage;
 
 import GraphMM.Dependency;
@@ -13,22 +21,74 @@ public class EdeltaDependencyAnalizer extends AbstractEdelta {
 
 	private static final GraphMMFactory graphFactory = GraphMMFactory.eINSTANCE;
 
-	public Repository analyzeEPackage(EPackage ePackage) {
-		var repository = graphFactory.createRepository();
-		analyzeEPackage(repository, ePackage);
+	public Repository analyzeEPackage(String path, String packageName) throws IOException {
+		try (var stream = Files.walk(Paths.get(path))) {
+			var resources = stream
+				.filter(
+					file -> {
+						return !Files.isDirectory(file);
+					})
+				.filter(
+					file -> {
+						return file.toString().endsWith(".ecore");
+					})
+				.map(file -> 
+					{
+						return loadEcoreFile(file.toString());
+					})
+				.collect(Collectors.toList());
+			var packages = resources.stream()
+				.map(r -> (EPackage) r.getContents().get(0))
+				.collect(Collectors.toList());
+			EPackage packageToAnalyze = packages.stream()
+				.filter(p -> packageName.equals(p.getName()))
+				.findFirst()
+				.orElseThrow(
+					() -> new IllegalArgumentException("No EPackage with name: " + packageName));
+			packages.remove(packageToAnalyze);
+			return analyzeEPackages(packageToAnalyze, packages);
+		}
+	}
+
+	private Repository analyzeEPackages(EPackage ePackage, List<EPackage> packages) {
+		var seen = new HashSet<EPackage>();
+		var repository = analyzeMainEPackage(ePackage, seen);
+		for (var p : packages) {
+			analyzeEPackage(repository, p, seen);
+		}
 		return repository;
 	}
 
-	private void analyzeEPackage(Repository repository, EPackage ePackage) {
-		var metamodel = createGraphMetamodelHighlighted(repository, ePackage);
+	public Repository analyzeMainEPackage(EPackage ePackage) {
+		return analyzeMainEPackage(ePackage, new HashSet<>());
+	}
+
+	private Repository analyzeMainEPackage(EPackage ePackage, Set<EPackage> seen) {
+		var repository = graphFactory.createRepository();
+		createGraphMetamodelHighlighted(repository, ePackage);
+		analyzeEPackage(repository, ePackage, seen);
+		return repository;
+	}
+
+	private void analyzeEPackage(Repository repository, EPackage ePackage, Set<EPackage> seen) {
+		if (seen.contains(ePackage))
+			return;
+		seen.add(ePackage);
+		var metamodel = createGraphMetamodel(repository, ePackage);
 		var usedPackages = EdeltaLibrary.usedPackages(ePackage);
 		for (var used : usedPackages) {
 			var usedMetamodel = createGraphMetamodel(repository, used);
-			var dependency = createDependency(repository, metamodel, usedMetamodel);
-			var secondLevel = EdeltaLibrary.usedPackages(used);
-			if (secondLevel.contains(ePackage)) {
-				dependency.setBidirectional(true);
-			}
+			repository.getEdges().stream()
+				.filter(Dependency.class::isInstance)
+				.map(Dependency.class::cast)
+				.filter(d -> ((Metamodel) d.getTrg()).getNsURI().equals(ePackage.getNsURI()))
+				.findFirst()
+				.ifPresentOrElse(
+					d -> d.setBidirectional(true),
+					() -> {
+						createDependency(repository, metamodel, usedMetamodel);
+					});
+			analyzeEPackage(repository, used, seen);
 		}
 	}
 
@@ -47,9 +107,16 @@ public class EdeltaDependencyAnalizer extends AbstractEdelta {
 	}
 
 	private Metamodel createGraphMetamodel(Repository repository, EPackage ePackage) {
-		var metamodel = createGraphMetamodel(ePackage);
-		repository.getNodes().add(metamodel);
-		return metamodel;
+		return repository.getNodes().stream()
+			.filter(Metamodel.class::isInstance)
+			.map(Metamodel.class::cast)
+			.filter(m -> m.getNsURI().equals(ePackage.getNsURI()))
+			.findFirst()
+			.orElseGet(() -> {
+				var metamodel = createGraphMetamodel(ePackage);
+				repository.getNodes().add(metamodel);
+				return metamodel;
+			});
 	}
 
 	private Metamodel createGraphMetamodel(EPackage ePackage) {
