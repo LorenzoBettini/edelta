3fce91117208655cd63844317fa26a37aac2c2a6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-05-26 14:56:51 +0200

    331: refactoring
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/331

--- edelta.parent/edelta.dependency.analyzer/src/edelta/dependency/analyzer/EdeltaDependencyAnalizer.java
+++ edelta.parent/edelta.dependency.analyzer/src/edelta/dependency/analyzer/EdeltaDependencyAnalizer.java
@@ -2,13 +2,17 @@ package edelta.dependency.analyzer;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
 
 import GraphMM.Dependency;
 import GraphMM.GraphMMFactory;
@@ -23,26 +27,35 @@ public class EdeltaDependencyAnalizer extends AbstractEdelta {
 
 	public Repository analyzeEPackage(String path, String packageName) throws IOException {
 		try (var stream = Files.walk(Paths.get(path))) {
-			var resources = stream
-				.filter(
-					file -> !Files.isDirectory(file))
-				.filter(
-					file -> file.toString().endsWith(".ecore"))
-				.map(file -> 
-					loadEcoreFile(file.toString()))
-				.collect(Collectors.toList());
-			var packages = resources.stream()
-				.map(r -> (EPackage) r.getContents().get(0))
-				.collect(Collectors.toList());
-			var packageToAnalyze = packages.stream()
-				.filter(p -> packageName.equals(p.getName()))
-				.findFirst()
-				.orElseThrow(
-					() -> new IllegalArgumentException
-						("No EPackage with name: " + packageName));
+			var resources = loadEcoreFiles(stream);
+			var packages = getEPackages(resources);
+			var packageToAnalyze = getEPackageByName(packages, packageName);
 			packages.remove(packageToAnalyze);
 			return analyzeEPackages(packageToAnalyze, packages);
 		}
+	}
+
+	private EPackage getEPackageByName(List<EPackage> packages, String packageName) {
+		return packages.stream()
+			.filter(p -> packageName.equals(p.getName()))
+			.findFirst()
+			.orElseThrow(
+				() -> new IllegalArgumentException
+					("No EPackage with name: " + packageName));
+	}
+
+	private List<EPackage> getEPackages(List<Resource> resources) {
+		return resources.stream()
+			.map(r -> (EPackage) r.getContents().get(0))
+			.collect(Collectors.toList());
+	}
+
+	private List<Resource> loadEcoreFiles(Stream<Path> stream) {
+		return stream
+			.filter(file -> !Files.isDirectory(file))
+			.filter(file -> file.toString().endsWith(".ecore"))
+			.map(file -> loadEcoreFile(file.toString()))
+			.collect(Collectors.toList());
 	}
 
 	private Repository analyzeEPackages(EPackage ePackage, List<EPackage> packages) {
@@ -65,19 +78,15 @@ public class EdeltaDependencyAnalizer extends AbstractEdelta {
 		return repository;
 	}
 
-	private void analyzeEPackage(Repository repository, EPackage ePackage, Set<EPackage> seen) {
-		if (seen.contains(ePackage))
+	private void analyzeEPackage(Repository repository, EPackage current, Set<EPackage> seen) {
+		if (seen.contains(current))
 			return;
-		seen.add(ePackage);
-		var metamodel = createGraphMetamodel(repository, ePackage);
-		var usedPackages = EdeltaLibrary.usedPackages(ePackage);
+		seen.add(current);
+		var metamodel = createGraphMetamodel(repository, current);
+		var usedPackages = EdeltaLibrary.usedPackages(current);
 		for (var used : usedPackages) {
 			var usedMetamodel = createGraphMetamodel(repository, used);
-			repository.getEdges().stream()
-				.filter(Dependency.class::isInstance)
-				.map(Dependency.class::cast)
-				.filter(d -> ((Metamodel) d.getTrg()).getNsURI().equals(ePackage.getNsURI()))
-				.findFirst()
+			findDependencyWithTarget(repository, current)
 				.ifPresentOrElse(
 					d -> d.setBidirectional(true),
 					() -> createDependency(repository, metamodel, usedMetamodel));
@@ -89,14 +98,6 @@ public class EdeltaDependencyAnalizer extends AbstractEdelta {
 		var metamodel = createGraphMetamodel(repository, ePackage);
 		metamodel.setHighlighted(true);
 		return metamodel;
-	}
-
-	private Dependency createDependency(Repository repository, Metamodel source, Metamodel target) {
-		var dependency = graphFactory.createDependency();
-		dependency.setSrc(source);
-		dependency.setTrg(target);
-		repository.getEdges().add(dependency);
-		return dependency;
 	}
 
 	private Metamodel createGraphMetamodel(Repository repository, EPackage ePackage) {
@@ -117,6 +118,22 @@ public class EdeltaDependencyAnalizer extends AbstractEdelta {
 		metamodel.setName(ePackage.getName());
 		metamodel.setNsURI(ePackage.getNsURI());
 		return metamodel;
+	}
+
+	private Optional<Dependency> findDependencyWithTarget(Repository repository, EPackage current) {
+		return repository.getEdges().stream()
+			.filter(Dependency.class::isInstance)
+			.map(Dependency.class::cast)
+			.filter(d -> ((Metamodel) d.getTrg()).getNsURI().equals(current.getNsURI()))
+			.findFirst();
+	}
+
+	private Dependency createDependency(Repository repository, Metamodel source, Metamodel target) {
+		var dependency = graphFactory.createDependency();
+		dependency.setSrc(source);
+		dependency.setTrg(target);
+		repository.getEdges().add(dependency);
+		return dependency;
 	}
 
 }
