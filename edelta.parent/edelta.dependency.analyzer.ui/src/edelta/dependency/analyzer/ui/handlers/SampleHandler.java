7cb1fa61a9917b55adba8925903fea2f71b06e76
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-06-04 16:17:41 +0200

    331: first prototype of dependency analysis context menu
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/331

---
+++ edelta.parent/edelta.dependency.analyzer.ui/src/edelta/dependency/analyzer/ui/handlers/SampleHandler.java
@@ -0,0 +1,67 @@
+package edelta.dependency.analyzer.ui.handlers;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.epsilon.picto.PictoView;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.xtext.util.StringInputStream;
+
+import edelta.dependency.analyzer.EdeltaDependencyAnalizer;
+
+public class SampleHandler extends AbstractHandler {
+
+	private static final String PICTO_FILE_CONTENTS = "<?nsuri picto?>\n"
+			+ "<picto\n"
+			+ "	transformation=\"platform:/plugin/edelta.dependency.analyzer.picto/picto/ecosystem2graphd3.egx\">\n"
+			+ "</picto>\n";
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		var selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
+		if (selection instanceof IStructuredSelection) {
+			var workspaceFile = (IFile) ((IStructuredSelection) selection).getFirstElement();
+			var fileSystemFile = workspaceFile.getLocation().toFile();
+			var path = fileSystemFile.getAbsolutePath();
+			var edeltaDependencyAnalizer = new EdeltaDependencyAnalizer();
+			var project = workspaceFile.getProject();
+			var projectPath = project.getLocation().toFile().getAbsolutePath();
+			System.out.println(path);
+			System.out.println(projectPath);
+			try {
+				var repository = edeltaDependencyAnalizer.analyzeEPackage(path);
+				var parentDirectoryName = fileSystemFile.getParentFile().getName();
+				var outputPath = "/analysis/results/" + parentDirectoryName;
+				var generatedModelName = workspaceFile.getName() + ".graphmm";
+				edeltaDependencyAnalizer.saveRepository(repository,
+					projectPath + outputPath, generatedModelName);
+				project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+				var generatedFile = project.getFile(outputPath + "/" + generatedModelName);
+				var generatedPictoFile = project.getFile(outputPath + "/" + generatedModelName + ".picto");
+				generatedPictoFile.delete(true, new NullProgressMonitor());
+				try (InputStream stream = new StringInputStream(PICTO_FILE_CONTENTS, generatedPictoFile.getCharset(true))) {
+					generatedPictoFile.create(stream, true, new NullProgressMonitor());
+				}
+				var page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				page.openEditor(new FileEditorInput(generatedFile), "org.eclipse.emf.ecore.presentation.ReflectiveEditorID");
+				page.showView(PictoView.ID);
+			} catch (IOException | CoreException e) {
+				throw new ExecutionException(e.getMessage(), e);
+			}
+			
+		}
+//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+//		MessageDialog.openInformation(window.getShell(), "Edelta Dependency Analyzer Ui", "Hello, Eclipse world");
+		return null;
+	}
+}
