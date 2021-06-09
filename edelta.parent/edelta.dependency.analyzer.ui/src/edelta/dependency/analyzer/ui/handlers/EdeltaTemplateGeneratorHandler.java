c2a0a3cde492aa400bed86f496c3f7a9f6c23c27
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-06-09 10:18:55 +0200

    331: context menu for generating template file
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/331

---
+++ edelta.parent/edelta.dependency.analyzer.ui/src/edelta/dependency/analyzer/ui/handlers/EdeltaTemplateGeneratorHandler.java
@@ -0,0 +1,66 @@
+package edelta.dependency.analyzer.ui.handlers;
+
+import java.io.InputStream;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.xtext.util.StringInputStream;
+
+import edelta.dependency.analyzer.EdeltaDependencyAnalizer;
+import edelta.dependency.analyzer.EdeltaDependencyAnalyzerUtils;
+import edelta.dependency.analyzer.ui.internal.EdeltaDependencyAnalyzerUiUtils;
+import edelta.ui.internal.EdeltaActivator;
+
+/**
+ * Runs the dependency analysis on the selected ecore file and the other ecore
+ * files in the same directory, creates a template edelta file.
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
+public class EdeltaTemplateGeneratorHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		EdeltaDependencyAnalyzerUiUtils.executeOnIFileSelection(event, workspaceFile -> {
+			var fileSystemFile = workspaceFile.getLocation().toFile();
+			var path = fileSystemFile.getAbsolutePath();
+			var edeltaDependencyAnalizer = new EdeltaDependencyAnalizer();
+			var repository = edeltaDependencyAnalizer.analyzeEPackage(path);
+			var dependencies = EdeltaDependencyAnalyzerUtils.computeMetamodelDependencies(repository);
+
+			var project = workspaceFile.getProject();
+			var outputPath = "src";
+			var workspaceFileName = workspaceFile.getName();
+			var generatedEdeltaFileName =
+				workspaceFileName.substring(0, workspaceFileName.lastIndexOf('.'))
+				+ "Template.edelta";
+
+			var generatedFile = project.getFile(outputPath + "/" + generatedEdeltaFileName);
+			generatedFile.delete(true, new NullProgressMonitor());
+			var contents = "// This is just an initial edelta file:\n" +
+					"// make sure you rename it before editing it\n" +
+					"// since it will be removed on the next run.\n\n";
+			contents += "metamodel \"" + dependencies.getHighlighted().getName() + "\"\n";
+			contents += dependencies.getDependencies().stream()
+					.map(d -> "metamodel \"" + d.getName() + "\"")
+					.collect(Collectors.joining("\n"));
+			try (InputStream stream = new StringInputStream(contents, generatedFile.getCharset(true))) {
+				generatedFile.create(stream, true, new NullProgressMonitor());
+			}
+
+			project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+
+			var page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			page.openEditor(new FileEditorInput(generatedFile),
+					EdeltaActivator.EDELTA_EDELTA);
+		});
+		return null;
+	}
+}
