7c92b9a37e859ce7a8943129fd92f6b0d1bbb62e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-09-10 15:20:43 +0200

    348: updated generated code in projects
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/348

--- edelta.parent/edelta.example/edelta-gen/edelta/ExampleWithDuplicatedFeatureCheck.java
+++ edelta.parent/edelta.example/edelta-gen/edelta/ExampleWithDuplicatedFeatureCheck.java
@@ -1,6 +1,7 @@
 package edelta;
 
 import edelta.lib.AbstractEdelta;
+import edelta.lib.EdeltaIssuePresenter;
 import edelta.lib.EdeltaLibrary;
 import edelta.refactorings.lib.EdeltaBadSmellsChecker;
 import java.util.function.Consumer;
@@ -16,6 +17,12 @@ public class ExampleWithDuplicatedFeatureCheck extends AbstractEdelta {
   
   public ExampleWithDuplicatedFeatureCheck() {
     checker = new EdeltaBadSmellsChecker(this);
+  }
+  
+  @Override
+  public void setIssuePresenter(final EdeltaIssuePresenter issuePresenter) {
+    super.setIssuePresenter(issuePresenter);
+    checker.setIssuePresenter(issuePresenter);
   }
   
   public ExampleWithDuplicatedFeatureCheck(final AbstractEdelta other) {
