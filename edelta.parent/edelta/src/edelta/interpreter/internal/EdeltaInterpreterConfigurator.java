927de6ef211cb69ca998adbeec60755b95347f66
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-09-27 19:15:40 +0200

    354: fixed ClassLoader configuration for Interpreter
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/354

--- edelta.parent/edelta/src/edelta/interpreter/internal/EdeltaInterpreterConfigurator.java
+++ edelta.parent/edelta/src/edelta/interpreter/internal/EdeltaInterpreterConfigurator.java
@@ -5,12 +5,17 @@ package edelta.interpreter.internal;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 
 import com.google.inject.Inject;
 
@@ -34,18 +39,22 @@ public class EdeltaInterpreterConfigurator {
 			Object context = ((XtextResourceSet) set).getClasspathURIContext();
 			if (context instanceof IJavaProject) {
 				try {
-					final IJavaProject jp = (IJavaProject) context;
-					String[] runtimeClassPath = JavaRuntime.computeDefaultRuntimeClassPath(jp);
-					URL[] urls = new URL[runtimeClassPath.length];
-					for (int i = 0; i < urls.length; i++) {
-						urls[i] = new URL(runtimeClassPath[i]);
+					final IJavaProject javaProject = (IJavaProject) context;
+					String[] classPathEntries = JavaRuntime.computeDefaultRuntimeClassPath(javaProject);
+					List<URL> urlList = new ArrayList<>();
+					for (int i = 0; i < classPathEntries.length; i++) {
+						String entry = classPathEntries[i];
+						IPath path = new Path(entry);
+						URL url;
+						url = path.toFile().toURI().toURL();
+						urlList.add(url);
 					}
 					URLClassLoader cl = new URLClassLoader(
-							urls,
+							urlList.toArray(new URL[urlList.size()]),
 							parentClassLoader);
 					interpreter.setClassLoader(cl);
 				} catch (Exception e) {
-					e.printStackTrace();
+					throw Exceptions.sneakyThrow(e);
 				}
 			}
 		}
