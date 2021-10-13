f5018bb7e9c28ff15b16e14ec87ca209576b54ac
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-10-13 18:19:45 +0200

    349: introduced EdeltaStandardLibrary
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/349

---
+++ edelta.parent/edelta.lib/src/edelta/lib/EdeltaStandardLibrary.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package edelta.lib;
+
+/**
+ * Standard library methods, for example, for adding, copying, moving
+ * elements.
+ * 
+ * @author Lorennzo Bettini
+ *
+ */
+public class EdeltaStandardLibrary extends AbstractEdelta {
+
+	public EdeltaStandardLibrary() {
+		super();
+	}
+
+	/**
+	 * @param other
+	 */
+	public EdeltaStandardLibrary(AbstractEdelta other) {
+		super(other);
+	}
+
+}
