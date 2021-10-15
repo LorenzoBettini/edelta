56274c9094fa20c707a961de62101cf82ef5baa6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-10-15 16:09:54 +0200

    349: Some library methods ported to standard library
    Task-Url: http://github.com/LorenzoBettini/edelta/issues/349

---
+++ edelta.parent/edelta.lib.tests/src/edelta/lib/tests/EdeltaStandardLibraryTest.java
@@ -0,0 +1,403 @@
+/**
+ * 
+ */
+package edelta.lib.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.emf.ecore.EcorePackage.Literals.EOBJECT;
+import static org.eclipse.emf.ecore.EcorePackage.Literals.ESTRING;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.Test;
+
+import edelta.lib.AbstractEdelta;
+import edelta.lib.EdeltaStandardLibrary;
+
+/**
+ * Library functions for manipulating an Ecore model.
+ * 
+ * @author Lorenzo Bettini
+ *
+ */
+public class EdeltaStandardLibraryTest {
+
+	private static final String MYPACKAGE = "mypackage";
+	private static final String MYOTHERPACKAGE = "myotherpackage";
+	private static final String MY2_ECORE = "My2.ecore";
+	private static final String MY_ECORE = "My.ecore";
+	private static final String TESTECORES = "testecores/";
+
+	private static EcoreFactory ecoreFactory = EcoreFactory.eINSTANCE;
+
+	private EdeltaStandardLibrary lib = new EdeltaStandardLibrary();
+
+	@Test
+	public void testGetEPackageWithOtherEdelta() {
+		AbstractEdelta other = new AbstractEdelta() {
+		};
+		lib = new EdeltaStandardLibrary(other);
+		// now lib and other share the same package manager
+		// so if we load something with lib we can get it with other
+		loadTestEcore(MY_ECORE);
+		loadTestEcore(MY2_ECORE);
+		EPackage ePackage = other.getEPackage(MYPACKAGE);
+		assertEquals(MYPACKAGE, ePackage.getName());
+		assertNotNull(ePackage);
+		assertNotNull(other.getEPackage(MYOTHERPACKAGE));
+		assertNull(other.getEPackage("foo"));
+	}
+
+	@Test
+	public void test_addEClass() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass eClass = ecoreFactory.createEClass();
+		lib.addEClass(ePackage, eClass);
+		assertSame(eClass,
+				ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewEClass() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass eClass = lib.addNewEClass(ePackage, "test");
+		assertEquals("test", eClass.getName());
+		assertSame(eClass,
+			ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewEClassWithInitializer() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass eClass = lib.addNewEClass(ePackage, "test",
+				cl -> {
+					assertNotNull(cl.getEPackage());
+					cl.setName("changed");
+				});
+		assertEquals("changed", eClass.getName());
+		assertSame(eClass,
+				ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewAbstractEClass() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass eClass = lib.addNewAbstractEClass(ePackage, "test");
+		assertEquals("test", eClass.getName());
+		assertThat(eClass.isAbstract())
+			.isTrue();
+		assertSame(eClass,
+			ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewAbstractEClassWithInitializer() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass eClass = lib.addNewAbstractEClass(ePackage, "test",
+				cl -> {
+					assertNotNull(cl.getEPackage());
+					cl.setName("changed");
+				});
+		assertEquals("changed", eClass.getName());
+		assertThat(eClass.isAbstract())
+			.isTrue();
+		assertSame(eClass,
+				ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addEEnum() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EEnum eEnum = ecoreFactory.createEEnum();
+		lib.addEEnum(ePackage, eEnum);
+		assertSame(eEnum,
+				ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewEEnum() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EEnum eEnum = lib.addNewEEnum(ePackage, "test");
+		assertEquals("test", eEnum.getName());
+		assertSame(eEnum,
+			ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewEEnumWithInitializer() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EEnum eEnum = lib.addNewEEnum(ePackage, "test",
+				cl -> {
+					assertNotNull(cl.getEPackage());
+					cl.setName("changed");
+				});
+		assertEquals("changed", eEnum.getName());
+		assertSame(eEnum,
+				ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addEEnumLiteral() {
+		EEnum eEnum = ecoreFactory.createEEnum();
+		EEnumLiteral eEnumLiteral = ecoreFactory.createEEnumLiteral();
+		lib.addEEnumLiteral(eEnum, eEnumLiteral);
+		assertSame(eEnumLiteral,
+				eEnum.getELiterals().get(0));
+	}
+
+	@Test
+	public void test_addNewEEnumLiteral() {
+		EEnum eEnum = ecoreFactory.createEEnum();
+		EEnumLiteral eEnumLiteral =
+				lib.addNewEEnumLiteral(eEnum, "test");
+		assertEquals("test", eEnumLiteral.getName());
+		assertSame(eEnumLiteral,
+				eEnum.getELiterals().get(0));
+	}
+
+	@Test
+	public void test_addNewEEnumLiteralWithInitializer() {
+		EEnum eEnum = ecoreFactory.createEEnum();
+		EEnumLiteral eEnumLiteral = lib.addNewEEnumLiteral(eEnum, "test",
+				lit -> {
+					assertNotNull(lit.getEEnum());
+					lit.setName("changed");
+				});
+		assertEquals("changed", eEnumLiteral.getName());
+		assertSame(eEnumLiteral,
+				eEnum.getELiterals().get(0));
+	}
+
+	@Test
+	public void test_addEDataType() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EDataType eEnum = ecoreFactory.createEDataType();
+		lib.addEDataType(ePackage, eEnum);
+		assertSame(eEnum,
+				ePackage.getEClassifiers().get(0));
+	}
+
+	@Test
+	public void test_addNewEDataType() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EDataType eDataType = lib.addNewEDataType(ePackage, "test", "java.lang.String");
+		assertEquals("test", eDataType.getName());
+		assertSame(eDataType,
+			ePackage.getEClassifiers().get(0));
+		assertEquals("java.lang.String", eDataType.getInstanceTypeName());
+		assertEquals("java.lang.String", eDataType.getInstanceClassName());
+		assertEquals(String.class, eDataType.getInstanceClass());
+	}
+
+	@Test
+	public void test_addNewEDataTypeWithInitializer() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EDataType eDataType = lib.addNewEDataType(ePackage, "test", "java.lang.String",
+				cl -> {
+					assertNotNull(cl.getEPackage());
+					cl.setName("changed");
+				});
+		assertEquals("changed", eDataType.getName());
+		assertSame(eDataType,
+			ePackage.getEClassifiers().get(0));
+		assertEquals("java.lang.String", eDataType.getInstanceTypeName());
+		assertEquals("java.lang.String", eDataType.getInstanceClassName());
+		assertEquals(String.class, eDataType.getInstanceClass());
+	}
+
+	@Test
+	public void test_addEAttribute() {
+		EClass eClass = ecoreFactory.createEClass();
+		EAttribute eAttribute = ecoreFactory.createEAttribute();
+		lib.addEAttribute(eClass, eAttribute);
+		assertSame(eAttribute,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addNewEAttribute() {
+		EClass eClass = ecoreFactory.createEClass();
+		EAttribute eAttribute =
+				lib.addNewEAttribute(eClass, "test", ESTRING);
+		assertEquals("test", eAttribute.getName());
+		assertEquals(ESTRING, eAttribute.getEType());
+		assertEquals(ESTRING, eAttribute.getEAttributeType());
+		assertSame(eAttribute,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addNewEAttributeWithInitializer() {
+		EClass eClass = ecoreFactory.createEClass();
+		EAttribute eAttribute =
+			lib.addNewEAttribute(eClass, "test", ESTRING,
+				attr -> {
+					assertNotNull(attr.getEContainingClass());
+					attr.setName("changed");
+				});
+		assertEquals("changed", eAttribute.getName());
+		assertEquals(ESTRING, eAttribute.getEType());
+		assertEquals(ESTRING, eAttribute.getEAttributeType());
+		assertSame(eAttribute,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addEReference() {
+		EClass eClass = ecoreFactory.createEClass();
+		EReference eReference = ecoreFactory.createEReference();
+		lib.addEReference(eClass, eReference);
+		assertSame(eReference,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addNewEReference() {
+		EClass eClass = ecoreFactory.createEClass();
+		EReference eReference =
+				lib.addNewEReference(eClass, "test", EOBJECT);
+		assertEquals("test", eReference.getName());
+		assertEquals(EOBJECT, eReference.getEType());
+		assertEquals(EOBJECT, eReference.getEReferenceType());
+		assertSame(eReference,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addNewEReferenceWithInitializer() {
+		EClass eClass = ecoreFactory.createEClass();
+		EReference eReference =
+			lib.addNewEReference(eClass, "test", EOBJECT,
+				ref -> {
+					assertNotNull(ref.getEContainingClass());
+					ref.setName("changed");
+				});
+		assertEquals("changed", eReference.getName());
+		assertEquals(EOBJECT, eReference.getEType());
+		assertEquals(EOBJECT, eReference.getEReferenceType());
+		assertSame(eReference,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addNewContainmentEReference() {
+		EClass eClass = ecoreFactory.createEClass();
+		EReference eReference =
+				lib.addNewContainmentEReference(eClass, "test", EOBJECT);
+		assertEquals("test", eReference.getName());
+		assertEquals(EOBJECT, eReference.getEType());
+		assertEquals(EOBJECT, eReference.getEReferenceType());
+		assertThat(eReference.isContainment()).isTrue();
+		assertSame(eReference,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addNewContainmentEReferenceWithInitializer() {
+		EClass eClass = ecoreFactory.createEClass();
+		EReference eReference =
+			lib.addNewContainmentEReference(eClass, "test", EOBJECT,
+				ref -> {
+					assertNotNull(ref.getEContainingClass());
+					ref.setName("changed");
+				});
+		assertEquals("changed", eReference.getName());
+		assertEquals(EOBJECT, eReference.getEType());
+		assertEquals(EOBJECT, eReference.getEReferenceType());
+		assertThat(eReference.isContainment()).isTrue();
+		assertSame(eReference,
+				eClass.getEStructuralFeatures().get(0));
+	}
+
+	@Test
+	public void test_addESuperType() {
+		EClass superClass = ecoreFactory.createEClass();
+		EClass subClass = ecoreFactory.createEClass();
+		lib.addESuperType(subClass, superClass);
+		assertThat(subClass.getESuperTypes()).containsExactly(superClass);
+	}
+
+	@Test
+	public void test_addNewSubclass() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass superClass = ecoreFactory.createEClass();
+		superClass.setName("Superclass");
+		ePackage.getEClassifiers().add(superClass);
+		EClass subClass = lib.addNewSubclass(superClass, "test");
+		assertEquals("test", subClass.getName());
+		assertThat(subClass.getESuperTypes())
+			.containsExactly(superClass);
+		assertThat(ePackage.getEClassifiers())
+			.containsExactlyInAnyOrder(superClass, subClass);
+	}
+
+	@Test
+	public void test_addNewSubclassWithInitializer() {
+		EPackage ePackage = ecoreFactory.createEPackage();
+		EClass superClass = ecoreFactory.createEClass();
+		superClass.setName("Superclass");
+		ePackage.getEClassifiers().add(superClass);
+		EClass subClass = lib.addNewSubclass(superClass, "test",
+				cl -> {
+					assertNotNull(cl.getEPackage());
+					cl.setName("changed");
+				});
+		assertEquals("changed", subClass.getName());
+		assertThat(subClass.getESuperTypes())
+			.containsExactly(superClass);
+		assertThat(ePackage.getEClassifiers())
+			.containsExactlyInAnyOrder(superClass, subClass);
+	}
+
+	@Test
+	public void test_removeESuperType() {
+		EClass superClass = ecoreFactory.createEClass();
+		EClass subClass = ecoreFactory.createEClass();
+		subClass.getESuperTypes().add(superClass);
+		assertThat(subClass.getESuperTypes()).containsExactly(superClass);
+		lib.removeESuperType(subClass, superClass);
+		assertThat(subClass.getESuperTypes()).isEmpty();
+	}
+
+	@Test
+	public void test_addNewESubpackage() {
+		EPackage superPackage = ecoreFactory.createEPackage();
+		EPackage subPackage =
+			lib.addNewESubpackage(superPackage, "newSubpackage", "prefix", "nsURI");
+		assertThat(subPackage)
+			.returns("newSubpackage", EPackage::getName)
+			.returns("prefix", EPackage::getNsPrefix)
+			.returns("nsURI", EPackage::getNsURI);
+		assertThat(superPackage.getESubpackages()).containsOnly(subPackage);
+		assertThat(subPackage.getESuperPackage()).isSameAs(superPackage);
+	}
+
+	@Test
+	public void test_addNewESubpackageWithInitializer() {
+		EPackage superPackage = ecoreFactory.createEPackage();
+		EPackage subPackage =
+			lib.addNewESubpackage(superPackage, "newSubpackage", "prefix", "nsURI",
+				(EPackage p) -> p.setName("changed"));
+		assertThat(subPackage)
+			.returns("changed", EPackage::getName)
+			.returns("prefix", EPackage::getNsPrefix)
+			.returns("nsURI", EPackage::getNsURI);
+		assertThat(superPackage.getESubpackages()).containsOnly(subPackage);
+		assertThat(subPackage.getESuperPackage()).isSameAs(superPackage);
+	}
+
+	private Resource loadTestEcore(String ecoreFile) {
+		return lib.loadEcoreFile(TESTECORES+ecoreFile);
+	}
+}
--- edelta.parent/edelta.lib/src/edelta/lib/EdeltaStandardLibrary.java
+++ edelta.parent/edelta.lib/src/edelta/lib/EdeltaStandardLibrary.java
@@ -3,6 +3,20 @@
  */
 package edelta.lib;
 
+import java.util.function.Consumer;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcoreFactory;
+
 /**
  * Standard library methods, for example, for adding, copying, moving
  * elements.
@@ -11,6 +25,8 @@ package edelta.lib;
  *
  */
 public class EdeltaStandardLibrary extends AbstractEdelta {
+
+	private static EcoreFactory ecoreFactory = EcoreFactory.eINSTANCE;
 
 	public EdeltaStandardLibrary() {
 		super();
@@ -21,6 +37,175 @@ public class EdeltaStandardLibrary extends AbstractEdelta {
 	 */
 	public EdeltaStandardLibrary(AbstractEdelta other) {
 		super(other);
+	}
+
+	public void addEStructuralFeature(EClass eClass, EStructuralFeature eStructuralFeature) {
+		eClass.getEStructuralFeatures().add(eStructuralFeature);
+	}
+
+	private void addEClassifier(EPackage ePackage, EClassifier eClassifier) {
+		ePackage.getEClassifiers().add(eClassifier);
+	}
+
+	public void addEClass(EPackage ePackage, EClass eClass) {
+		addEClassifier(ePackage, eClass);
+	}
+
+	public EClass addNewEClass(EPackage ePackage, String name) {
+		return addNewEClass(ePackage, name, null);
+	}
+
+	public EClass addNewEClass(EPackage ePackage, String name, Consumer<EClass> initializer) {
+		var c = ecoreFactory.createEClass();
+		c.setName(name);
+		addEClass(ePackage, c);
+		safeRunInitializer(initializer, c);
+		return c;
+	}
+
+	public EClass addNewAbstractEClass(EPackage ePackage, String name) {
+		return addNewAbstractEClass(ePackage, name, null);
+	}
+
+	public EClass addNewAbstractEClass(EPackage ePackage, String name, Consumer<EClass> initializer) {
+		return addNewEClass(ePackage, name, c -> {
+			c.setAbstract(true);
+			safeRunInitializer(initializer, c);
+		});
+	}
+
+	public void addEEnum(EPackage ePackage, EEnum eEnum) {
+		addEClassifier(ePackage, eEnum);
+	}
+
+	public EEnum addNewEEnum(EPackage ePackage, String name) {
+		return addNewEEnum(ePackage, name, null);
+	}
+
+	public EEnum addNewEEnum(EPackage ePackage, String name, Consumer<EEnum> initializer) {
+		var e = ecoreFactory.createEEnum();
+		e.setName(name);
+		addEEnum(ePackage, e);
+		safeRunInitializer(initializer, e);
+		return e;
+	}
+
+	public void addEEnumLiteral(EEnum eEnum, EEnumLiteral eEnumLiteral) {
+		eEnum.getELiterals().add(eEnumLiteral);
+	}
+
+	public EEnumLiteral addNewEEnumLiteral(EEnum eEnum, String name) {
+		return addNewEEnumLiteral(eEnum, name, null);
+	}
+
+	public EEnumLiteral addNewEEnumLiteral(EEnum eEnum, String name, Consumer<EEnumLiteral> initializer) {
+		var e = ecoreFactory.createEEnumLiteral();
+		e.setName(name);
+		addEEnumLiteral(eEnum, e);
+		safeRunInitializer(initializer, e);
+		return e;
+	}
+
+	public void addEDataType(EPackage ePackage, EDataType eDataType) {
+		addEClassifier(ePackage, eDataType);
+	}
+
+	public EDataType addNewEDataType(EPackage ePackage, String name, String instanceTypeName) {
+		return addNewEDataType(ePackage, name, instanceTypeName, null);
+	}
+
+	public EDataType addNewEDataType(EPackage ePackage, String name, String instanceTypeName, Consumer<EDataType> initializer) {
+		var e = ecoreFactory.createEDataType();
+		e.setName(name);
+		e.setInstanceTypeName(instanceTypeName);
+		addEDataType(ePackage, e);
+		safeRunInitializer(initializer, e);
+		return e;
+	}
+
+	public void addEAttribute(EClass eClass, EAttribute eAttribute) {
+		addEStructuralFeature(eClass, eAttribute);
+	}
+
+	public EAttribute addNewEAttribute(EClass eClass, String name, EDataType dataType) {
+		return addNewEAttribute(eClass, name, dataType, null);
+	}
+
+	public EAttribute addNewEAttribute(EClass eClass, String name, EDataType dataType, Consumer<EAttribute> initializer) {
+		var e = ecoreFactory.createEAttribute();
+		e.setName(name);
+		e.setEType(dataType);
+		addEAttribute(eClass, e);
+		safeRunInitializer(initializer, e);
+		return e;
+	}
+
+	public void addEReference(EClass eClass, EReference eReference) {
+		addEStructuralFeature(eClass, eReference);
+	}
+
+	public EReference addNewEReference(EClass eClass, String name, EClass referenceType) {
+		return addNewEReference(eClass, name, referenceType, null);
+	}
+
+	public EReference addNewEReference(EClass eClass, String name, EClass referenceType, Consumer<EReference> initializer) {
+		var e = ecoreFactory.createEReference();
+		e.setName(name);
+		e.setEType(referenceType);
+		addEReference(eClass, e);
+		safeRunInitializer(initializer, e);
+		return e;
+	}
+
+	public EReference addNewContainmentEReference(EClass eClass, String name, EClass referenceType) {
+		return addNewContainmentEReference(eClass, name, referenceType, null);
+	}
+
+	public EReference addNewContainmentEReference(EClass eClass, String name, EClass referenceType, Consumer<EReference> initializer) {
+		return addNewEReference(eClass, name, referenceType, r -> {
+			r.setContainment(true);
+			safeRunInitializer(initializer, r);
+		});
+	}
+
+	public void addESuperType(EClass subClass, EClass superClass) {
+		subClass.getESuperTypes().add(superClass);
+	}
+
+	public EClass addNewSubclass(EClass superClass, String name) {
+		return addNewSubclass(superClass, name, null);
+	}
+
+	public EClass addNewSubclass(EClass superClass, String name, Consumer<EClass> initializer) {
+		return addNewEClass(superClass.getEPackage(), name, c -> {
+			c.getESuperTypes().add(superClass);
+			safeRunInitializer(initializer, c);
+		});
+	}
+
+	public void removeESuperType(EClass subClass, EClass superClass) {
+		subClass.getESuperTypes().remove(superClass);
+	}
+
+	public EPackage addNewESubpackage(EPackage superPackage, String name, String nsPrefix, String nsURI) {
+		return addNewESubpackage(superPackage, name, nsPrefix, nsURI, null);
+	}
+
+	public EPackage addNewESubpackage(EPackage superPackage, String name, String nsPrefix, String nsURI,
+			Consumer<EPackage> initializer) {
+		var newSubpackage = ecoreFactory.createEPackage();
+		newSubpackage.setName(name);
+		newSubpackage.setNsPrefix(nsPrefix);
+		newSubpackage.setNsURI(nsURI);
+		superPackage.getESubpackages().add(newSubpackage);
+		safeRunInitializer(initializer, newSubpackage);
+		return newSubpackage;
+	}
+
+	private <T extends ENamedElement> void safeRunInitializer(Consumer<T> initializer, T e) {
+		if (initializer != null) {
+			initializer.accept(e);
+		}
 	}
 
 }
