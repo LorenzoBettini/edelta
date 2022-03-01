093202cad49fccaa65f44e9c37e95a95c6bf86d7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-03-01 19:32:56 +0100

    use job manager for FAMILY_AUTO_BUILD

--- edelta.parent/edelta.swtbot.tests/src/edelta/swtbot/tests/EdeltaProjectWizardSwtBotTest.java
+++ edelta.parent/edelta.swtbot.tests/src/edelta/swtbot/tests/EdeltaProjectWizardSwtBotTest.java
@@ -1,10 +1,12 @@
 package edelta.swtbot.tests;
 
 import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
-import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.waitForBuild;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
@@ -20,7 +22,7 @@ import org.junit.runner.RunWith;
 public class EdeltaProjectWizardSwtBotTest extends EdeltaAbstractSwtbotTest {
 
 	@Test
-	public void canCreateANewProject() throws CoreException {
+	public void canCreateANewProject() throws CoreException, OperationCanceledException, InterruptedException {
 		bot.menu("File").menu("New").menu("Project...").click();
 
 		SWTBotShell shell = bot.shell("New Project");
@@ -37,9 +39,14 @@ public class EdeltaProjectWizardSwtBotTest extends EdeltaAbstractSwtbotTest {
 		bot.waitUntil(shellCloses(shell), SWTBotPreferences.TIMEOUT);
 		assertTrue("Project doesn't exist: " + TEST_PROJECT, isProjectCreated(TEST_PROJECT));
 
-		System.out.println("Waiting for build...");
-		waitForBuild();
+//		System.out.println("Waiting for build...");
+//		waitForBuild();
+		System.out.println("Waiting for auto build...");
+		Job.getJobManager().join(ResourcesPlugin.FAMILY_AUTO_BUILD, null);
 		assertErrorsInProject(0);
+		getProjectTreeItem(TEST_PROJECT)
+			.expand()
+			.expandNode("edelta-gen", "com.example", "Example.java");
 	}
 
 }
