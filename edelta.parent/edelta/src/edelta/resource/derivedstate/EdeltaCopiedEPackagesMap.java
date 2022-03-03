9c6dd445d2ef28b263d1addddf33bec26ac7232e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-03-03 19:47:53 +0100

    simplified original element recorder

--- edelta.parent/edelta/src/edelta/resource/derivedstate/EdeltaCopiedEPackagesMap.java
+++ edelta.parent/edelta/src/edelta/resource/derivedstate/EdeltaCopiedEPackagesMap.java
@@ -4,7 +4,11 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+
+import com.google.common.collect.HashBiMap;
 
 /**
  * Map of copied {@link EPackage}.
@@ -15,11 +19,13 @@ import org.eclipse.emf.ecore.EPackage;
 public class EdeltaCopiedEPackagesMap {
 
 	private Map<String, EPackage> packagesByName = new HashMap<>();
+	private Map<EObject,EObject> inverse;
 
-	public void setCopies(Collection<EPackage> copies) {
+	public void setCopies(Collection<EPackage> copies, Map<EObject, EObject> copyMap) {
 		for (var copy : copies) {
 			packagesByName.computeIfAbsent(copy.getName(), key -> copy);
 		}
+		inverse = HashBiMap.create(copyMap).inverse();
 	}
 
 	public Collection<EPackage> values() {
@@ -32,5 +38,11 @@ public class EdeltaCopiedEPackagesMap {
 
 	public EPackage get(String name) {
 		return packagesByName.get(name);
+	}
+
+	public ENamedElement getOriginal(ENamedElement e) {
+		if (e == null)
+			return null;
+		return (ENamedElement) inverse.get(e);
 	}
 }
