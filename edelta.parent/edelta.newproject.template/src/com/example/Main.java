51101a14ef0e12f8e2710ac30e6f548dc18fb4db
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-03-10 12:34:03 +0100

    also use the migrator in the template

--- edelta.parent/edelta.newproject.template/src/com/example/Main.java
+++ edelta.parent/edelta.newproject.template/src/com/example/Main.java
@@ -9,9 +9,12 @@ public class Main {
 		EdeltaEngine engine = new EdeltaEngine(Example::new);
 		// Make sure you load all the used Ecores (Ecore.ecore is always loaded)
 		engine.loadEcoreFile("model/My.ecore");
+		// In case, also load model files
+		engine.loadModelFile("model/MyClass.xmi");
+		engine.loadModelFile("model/MyRoot.xmi");
 		// Execute the actual transformations defined in the DSL
 		engine.execute();
-		// Save the modified Ecore model into a new path
+		// Save the modified Ecores and models into a new path
 		engine.save("modified");
 	}
 }
