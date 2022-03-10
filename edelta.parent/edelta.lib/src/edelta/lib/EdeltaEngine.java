5fbbd0824066e273740c9a52cfd3732290d34696
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-03-10 09:55:44 +0100

    refactoring and documentation

--- edelta.parent/edelta.lib/src/edelta/lib/EdeltaEngine.java
+++ edelta.parent/edelta.lib/src/edelta/lib/EdeltaEngine.java
@@ -1,7 +1,7 @@
 package edelta.lib;
 
 import java.io.IOException;
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import org.eclipse.emf.ecore.resource.Resource;
 
@@ -14,11 +14,19 @@ import org.eclipse.emf.ecore.resource.Resource;
  */
 public class EdeltaEngine {
 
-	public static interface EdeltaRuntimeProvider extends Function<AbstractEdelta, AbstractEdelta> {
+	/**
+	 * The functional interface representing the constructor argument.
+	 * 
+	 * @author Lorenzo Bettini
+	 *
+	 */
+	@FunctionalInterface
+	public static interface EdeltaRuntimeProvider extends UnaryOperator<AbstractEdelta> {
 	}
 
-	private EdeltaModelManager originalModelManager = new EdeltaModelManager();
 	private EdeltaRuntimeProvider provider;
+
+	private EdeltaModelManager originalModelManager = new EdeltaModelManager();
 	private EdeltaModelManager evolvingModelManager;
 
 	public EdeltaEngine(EdeltaRuntimeProvider provider) {
@@ -33,6 +41,15 @@ public class EdeltaEngine {
 		return originalModelManager.loadModelFile(path);
 	}
 
+	/**
+	 * Executes the actual evolution of the loaded Ecore files and of the (possibly)
+	 * loaded model files.
+	 * 
+	 * Such an evolution is NOT performed on the loaded Ecore and model files: it is
+	 * executed on copies.
+	 * 
+	 * @throws Exception
+	 */
 	public void execute() throws Exception {
 		var migrator = new EdeltaModelMigrator(originalModelManager);
 		evolvingModelManager = migrator.getEvolvingModelManager();
@@ -41,6 +58,12 @@ public class EdeltaEngine {
 		migrator.copyModels();
 	}
 
+	/**
+	 * Saves the evolved Ecore and model files to the specified outputPath.
+	 * 
+	 * @param outputPath
+	 * @throws IOException
+	 */
 	public void save(String outputPath) throws IOException {
 		evolvingModelManager.saveEcores(outputPath);
 		evolvingModelManager.saveModels(outputPath);
