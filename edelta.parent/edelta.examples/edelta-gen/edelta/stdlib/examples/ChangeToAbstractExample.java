f01891a0403b825ad3584c3b345758e510e93d88
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-04-04 18:22:24 +0200

    also remove the manager attribute

--- edelta.parent/edelta.examples/edelta-gen/edelta/stdlib/examples/ChangeToAbstractExample.java
+++ edelta.parent/edelta.examples/edelta-gen/edelta/stdlib/examples/ChangeToAbstractExample.java
@@ -4,6 +4,7 @@ import edelta.lib.EdeltaDefaultRuntime;
 import edelta.lib.EdeltaEcoreUtil;
 import edelta.lib.EdeltaModelMigrator;
 import edelta.lib.EdeltaRuntime;
+import edelta.lib.EdeltaUtils;
 import java.util.function.Consumer;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
@@ -21,9 +22,10 @@ public class ChangeToAbstractExample extends EdeltaDefaultRuntime {
     final EClass employee = this.stdLib.addNewSubclass(personClass, "Employee");
     final EClass manager = this.stdLib.addNewSubclass(personClass, "Manager");
     personClass.setAbstract(true);
+    final EAttribute managerAttribute = getEAttribute("PersonListForChangeToAbstract", "Person", "manager");
     final Consumer<EdeltaModelMigrator> _function = (EdeltaModelMigrator it_1) -> {
       final EdeltaModelMigrator.EObjectFunction _function_1 = (EObject oldObj) -> {
-        final EAttribute managerFeature = it_1.<EAttribute>getOriginal(getEAttribute("PersonListForChangeToAbstract", "Person", "manager"));
+        final EAttribute managerFeature = it_1.<EAttribute>getOriginal(managerAttribute);
         Object _eGet = oldObj.eGet(managerFeature);
         if ((((Boolean) _eGet)).booleanValue()) {
           return EdeltaEcoreUtil.createInstance(manager);
@@ -34,6 +36,7 @@ public class ChangeToAbstractExample extends EdeltaDefaultRuntime {
         it_1.<EClass>isRelatedTo(personClass), _function_1);
     };
     this.modelMigration(_function);
+    EdeltaUtils.removeElement(managerAttribute);
   }
   
   @Override
