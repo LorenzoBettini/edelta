7477fbf97d5b921eaa8d252bbb7da79e4f3cf9d1
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-04-04 12:09:00 +0200

    re-added generated files in maven examples
    were removed by mistake!

---
+++ edelta.parent/edelta.maven.example/edelta-gen/com/example/ExampleReusableFunctions.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import edelta.lib.EdeltaDefaultRuntime;
+import edelta.lib.EdeltaRuntime;
+import edelta.lib.EdeltaUtils;
+import java.util.function.Consumer;
+import org.eclipse.emf.ecore.EClass;
+
+@SuppressWarnings("all")
+public class ExampleReusableFunctions extends EdeltaDefaultRuntime {
+  public ExampleReusableFunctions(final EdeltaRuntime other) {
+    super(other);
+  }
+  
+  public EClass createClassWithSubClass(final String name, final EClass superClass) {
+    final Consumer<EClass> _function = (EClass it) -> {
+      this.stdLib.addESuperType(it, superClass);
+    };
+    return EdeltaUtils.newEClass(name, _function);
+  }
+  
+  @Override
+  public void performSanityChecks() throws Exception {
+    ensureEPackageIsLoaded("ecore");
+  }
+}
