46f02a3b88593c1450721f6f4a5f12c0677c8319
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-04-05 14:09:08 +0200

    don't use ecoreref in modelMigration example

--- edelta.parent/edelta.examples/edelta-gen/edelta/stdlib/examples/ChangeReferenceTypeManualExample.java
+++ edelta.parent/edelta.examples/edelta-gen/edelta/stdlib/examples/ChangeReferenceTypeManualExample.java
@@ -5,9 +5,11 @@ import edelta.lib.EdeltaEcoreUtil;
 import edelta.lib.EdeltaModelMigrator;
 import edelta.lib.EdeltaRuntime;
 import java.util.function.Consumer;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 @SuppressWarnings("all")
@@ -17,11 +19,13 @@ public class ChangeReferenceTypeManualExample extends EdeltaDefaultRuntime {
   }
   
   public void exampleOfChangeReferenceType(final EPackage it) {
+    final EAttribute nameElementValue = getEAttribute("PersonListForChangeType", "NameElement", "nameElementValue");
     final EClass otherNameElement = this.stdLib.addNewEClassAsSibling(getEClass("PersonListForChangeType", "NameElement"), "OtherNameElement");
-    this.stdLib.copyToAs(
+    final EStructuralFeature otherNameElementValue = this.stdLib.copyToAs(
       getEAttribute("PersonListForChangeType", "NameElement", "nameElementValue"), otherNameElement, 
       "otherNameElementValue");
-    getEReference("PersonListForChangeType", "Person", "firstName").setEType(otherNameElement);
+    final EReference firstName = getEReference("PersonListForChangeType", "Person", "firstName");
+    firstName.setEType(otherNameElement);
     final Consumer<EdeltaModelMigrator> _function = (EdeltaModelMigrator it_1) -> {
       final EdeltaModelMigrator.CopyProcedure _function_1 = (EStructuralFeature oldFeature, EObject oldObj, EObject newObj) -> {
         boolean _eIsSet = oldObj.eIsSet(oldFeature);
@@ -31,15 +35,15 @@ public class ChangeReferenceTypeManualExample extends EdeltaDefaultRuntime {
         }
         final EObject oldReferred = it_1.getMigrated(
           EdeltaEcoreUtil.getValueAsEObject(oldObj, oldFeature));
-        final Object oldValue = oldReferred.eGet(getEAttribute("PersonListForChangeType", "NameElement", "nameElementValue"));
+        final Object oldValue = oldReferred.eGet(nameElementValue);
         final Consumer<EObject> _function_2 = (EObject newRef) -> {
-          newRef.eSet(getEAttribute("PersonListForChangeType", "OtherNameElement", "otherNameElementValue"), oldValue);
+          newRef.eSet(otherNameElementValue, oldValue);
         };
-        newObj.eSet(getEReference("PersonListForChangeType", "Person", "firstName"), 
+        newObj.eSet(firstName, 
           EdeltaEcoreUtil.createInstance(otherNameElement, _function_2));
       };
       it_1.copyRule(
-        it_1.<EStructuralFeature>isRelatedTo(getEReference("PersonListForChangeType", "Person", "firstName")), _function_1);
+        it_1.<EStructuralFeature>isRelatedTo(firstName), _function_1);
     };
     this.modelMigration(_function);
   }
