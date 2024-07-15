6c398b775d481a5623eaf6e8ed493f21c73ddd1d
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-07-15 12:08:16 +0200

    fixed sonar issues

--- edelta.parent/edelta.examples/tests/edelta/examples/tests/AbstractEdeltaExamplesWithPromptTest.java
+++ edelta.parent/edelta.examples/tests/edelta/examples/tests/AbstractEdeltaExamplesWithPromptTest.java
@@ -2,7 +2,6 @@ package edelta.examples.tests;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 
@@ -14,17 +13,16 @@ import edelta.refactorings.lib.helper.EdeltaPromptHelper;
 
 public abstract class AbstractEdeltaExamplesWithPromptTest extends AbstractEdeltaExamplesTest {
 
-	private ByteArrayOutputStream outContent;
-	private ByteArrayOutputStream errContent;
-	private ByteArrayInputStream testIn;
-	private final PrintStream originalOut = System.out;
-	private final PrintStream originalErr = System.err;
-	private final InputStream originalIn = System.in;
+	private static final PrintStream ORIGINAL_OUT = System.out; // NOSONAR: we need this
+	private static final PrintStream ORIGINAL_ERR = System.err; // NOSONAR: we need this
+	private static final InputStream ORIGINAL_IN = System.in;
 
 	private Logger logger = Logger.getLogger(getClass());
 
 	@BeforeEach
-	void setup() throws Exception {
+	void setup() {
+		ByteArrayOutputStream errContent;
+		ByteArrayOutputStream outContent;
 		EdeltaPromptHelper.close();
 		outContent = new ByteArrayOutputStream();
 		errContent = new ByteArrayOutputStream();
@@ -33,15 +31,16 @@ public abstract class AbstractEdeltaExamplesWithPromptTest extends AbstractEdelt
 	}
 
 	@AfterEach
-	void resetStreams() throws IOException {
+	void resetStreams() {
 		logger.info("Resetting streams");
 		EdeltaPromptHelper.close();
-		System.setOut(originalOut);
-		System.setErr(originalErr);
-		System.setIn(originalIn);
+		System.setOut(ORIGINAL_OUT);
+		System.setErr(ORIGINAL_ERR);
+		System.setIn(ORIGINAL_IN);
 	}
 
 	protected void enterInput(String data) {
+		ByteArrayInputStream testIn;
 		testIn = new ByteArrayInputStream(data.getBytes());
 		System.setIn(testIn);
 	}
